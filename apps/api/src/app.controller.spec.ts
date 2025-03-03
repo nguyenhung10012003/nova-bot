031fc18469304c892073d1985ea0a8ebe628a857
Nguyễn Đức Hùng <71268621+nguyenhung10012003@users.noreply.github.com>
2025-03-03 14:25:26 +0000

    add integration card component

--- apps/api/src/app.controller.spec.ts
+++ apps/api/src/app.controller.spec.ts
@@ -1,23 +1,25 @@
+import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 import { Test, TestingModule } from '@nestjs/testing';
+import { getConfig } from '../dist/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SchedulerService } from './scheduler/scheduler.service';
-import { ConfigModule } from '@nestjs/config';
-import { config } from '../dist/config';
-import { ScheduleModule } from '@nestjs/schedule';
 import { PrismaModule } from './prisma/prisma.module';
+import { SchedulerService } from './scheduler/scheduler.service';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
-      imports: [ConfigModule.forRoot({
-            isGlobal: true,
-            load: [() => config],
-          }),
-          ScheduleModule.forRoot(),
-          PrismaModule,],
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          load: [getConfig],
+        }),
+        ScheduleModule.forRoot(),
+        PrismaModule,
+      ],
       controllers: [AppController],
       providers: [AppService, SchedulerService],
     }).compile();
--- apps/web/app/dashboard/[chatflowId]/connect/page.tsx
+++ apps/web/app/dashboard/[chatflowId]/connect/page.tsx
@@ -1,3 +1,40 @@
-export default function Page() {
-  return <></>;
+import { IntegrationCard } from '@/components/integration/integration-card';
+import { Card } from '@nova/ui/components/ui/card';
+
+export default function Page({
+  params,
+  searchParams,
+}: {
+  params: {
+    chatflowId: string;
+  };
+  searchParams?: { [key: string]: string | string[] | undefined };
+}) {
+  if (searchParams?.channel === 'fb') {
+    return <Card></Card>;
+  } else if (searchParams?.channel === 'telegram') {
+    return <></>;
+  } else {
+    const integrations = [
+      {
+        title: 'Facebook Messenger',
+        img: '/images/messenger.png',
+        description: 'Connect chatbot with Facebook Page to auto reply customer messages',
+        url: `/dashboard/${params.chatflowId}/connect?channel=fb`
+      },
+      {
+        title: 'Telegram',
+        img: '/images/telegram.png',
+        description: 'Connect chatbot with Telegram bot to auto reply customer messages',
+        url: `/dashboard/${params.chatflowId}/connect?channel=telegram`
+      }
+    ]
+    return (
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-4 py-2 gap-4">
+        {integrations.map((integration, index) => (
+          <IntegrationCard {...integration} key={index}/>
+        ))}
+      </div>
+    );
+  }
 }
